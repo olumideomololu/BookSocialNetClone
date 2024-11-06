0816ec27a461e58f68091427fd681ed2127e8753
olumideomololu <oaomololu@gmail.com>
2024-11-06 16:23:37 +0000

    Initial Spring Boot Application Generated

---
+++ book-social-network/src/test/java/com/oaomololu/bsn/BookSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oaomololu.bsn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
